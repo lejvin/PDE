30c13f74a21fba8af7ac09059635b0f728eafea2
Lukas Ejvinsson <lukas.ejvinsson@gmail.com>
2025-10-08 14:26:00 +0200

    create mydft function

---
+++ mydft.m
@@ -0,0 +1,13 @@
+function z = mydft(y)
+% Compute the DFT of a vector y of length N
+
+N = length(y);
+j = 0:(N-1); % row vector containing the indexes needed for the sum
+n = j'; % column vector containing indexes, needed to use matrix multiplication instead of for loops
+omega = exp (2*pi*1i/N); % here i is the imaginary unit
+
+
+z = (1/N) .* omega.^(-n*j)*y'; 
+
+
+
