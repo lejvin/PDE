8d034f5fb0e0300d204fdf0d16207a7c9291f02d
Lukas Ejvinsson <lukas.ejvinsson@gmail.com>
2025-10-22 12:44:09 +0200

    fix myfouriercoeff

--- myfouriercoeff.m
+++ myfouriercoeff.m
@@ -6,8 +6,13 @@ function [a0,a,b] = myfouriercoeff(z)
 % a: vector with the Fourier coeﬃcients an
 % b: vector with the Fourier coeﬃcients bn
 N = length(z);% Compute the length of vector z
-c= z(2:floor(N/2));% remove ﬁrst element z(1) from z-vector
+
+c= z(2:floor(N/2)+1);% remove ﬁrst element z(1) from z-vector
 a0 = z(1);
 
 a = c + conj(c);
 b = 1i * (c - conj(c));
+
+subtract = mod(N+1,2);
+a = [a flip(a(1:end-subtract))];
+b = [b flip(b(1:end-subtract))];
